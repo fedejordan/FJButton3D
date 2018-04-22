bdbe2d0d1ec5d707fe5f8067740f4c999fc99ca8
Federico Jordán <federicojordan@MacBook-Pro-de-Federico-Jordan.local>
2018-04-22 17:54:45 -0300

    added toggle example

--- FJButton3DDemo/FJButton3DDemo/ViewController.swift
+++ FJButton3DDemo/FJButton3DDemo/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FJButton3DDelegate {
 
     @IBOutlet private weak var button3D: FJButton3D!
+    @IBOutlet private weak var toggleButton3D: FJButton3D!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        toggleButton3D.style = .pressed
+        button3D.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func didTap(onButton3D button3d: FJButton3D) {
+        toggleButton3D.pressed = false
+    }
 
 }
 
